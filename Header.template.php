506b146db0a12e7d3d5c323ee2e75b44d4e3b726
Nalithdev <Ethan.delcroix@outlook.fr>
2022-12-02 11:09:55 +0100

    correction burger

--- Header.template.php
+++ Header.template.php
@@ -1,7 +1,7 @@
 <header class="flex">
 
-    <div id="mySidenav" class="block h-0 w-full fixed z-{1} top-0 right-0 bg-black overflow-hidden duration-{0.5s} ">
-        <a href="" class="closebtn" onclick="closeNav()">&times;</a>
+    <div id="mySidenav" class="sidenav block w-full fixed z-{1} top-0 right-0 bg-black overflow-hidden duration-{0.5s} ">
+        <a href="javascript:void(0)" class="closebtn w-1/12" onclick="closeNav()">&times;</a>
 
         <a href="#About"  class="underline " >About me</a>
         <a href="#Skills"  class="underline">Skills</a>
